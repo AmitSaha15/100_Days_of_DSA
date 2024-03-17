a1271c9ddf90bb754013140e20e6c5e457e91b7e
Amit Saha <asaha.bwn2000@gmail.com>
2024-03-17 22:58:34 +0530

    Day89 completed

--- Day89/leetcodeQsn67.java
+++ Day89/leetcodeQsn67.java
@@ -4,12 +4,12 @@ package Day89;
 
 public class leetcodeQsn67 {
     public String addBinary(String a, String b) {
-        StringBuilder ans = new StringBuilder();
-        int carry = 0;
-        int i = a.length() - 1;
-        int j = b.length() - 1;
-  
-        while (i>=0 || j>=0 || carry==1) {
+      StringBuilder ans = new StringBuilder();
+      int carry = 0;
+      int i = a.length() - 1;
+      int j = b.length() - 1;
+
+      while (i>=0 || j>=0 || carry==1) {
           if (i>=0){
               carry += a.charAt(i) - '0';
               i--;
@@ -18,11 +18,11 @@ public class leetcodeQsn67 {
               carry += b.charAt(j) - '0';
               j--;
           }
-  
+
           ans.append(carry%2);
           carry /= 2;
-        }
-  
-        return ans.reverse().toString();
       }
+
+      return ans.reverse().toString();
+    }
 }
